a22195e08c5db280df8f2cd7d03cf4fdf328ffd1
Ngozi <hexwhyzed01@gmail.com>
2026-01-24 16:05:28 +0100

    Set up database event listener

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 
 port = process.env.PORT;
 
+db.once("connection", () => {
+  console.log("Database on!");
+});
+db.on("error", (error) => {
+  console.log("Error", error);
+});
+db.on("disconnected", () => {
+  console.log("MBD Disconnected");
+});
+
 app.listen((port) => {
   console.log("Successfully connected to server");
 });
