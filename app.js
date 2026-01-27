87765b9ad54350505a9c629b0e960bb4a0d402c2
Ngozi <hexwhyzed01@gmail.com>
2026-01-27 19:47:57 +0100

    Add the transaction route middlware, and a stack to the global error handler

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 
 const db = require("./database/main");
 const authRoute = require("./routes/auth.route");
+const transactionRoute = require("./routes/transaction.route")
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", transactionRoute)
 app.use("/api", authRoute);
 
 app.use("/", (req, res, next) => {
@@ -32,10 +34,12 @@ app.use(((err, req, res, next) => {
 app.use((err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Internal Server Error";
+  let stack = err.stack
   return res.status(statusCode).json({
     success: false,
     "status code": statusCode,
     message: message,
+    stack: err.stack
   });
 });
 
