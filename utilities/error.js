0b97caf48cb4bf43bcf02bb63fefe70d62da2f58
Ngozi <hexwhyzed01@gmail.com>
2026-01-30 23:41:57 +0100

    Add stack to the error  function to spot location of the erroe

--- utilities/error.js
+++ utilities/error.js
@@ -2,7 +2,8 @@ const errorHandler = (statusCode, message) => {
   let err = new Error;
   err.statusCode = statusCode;
   err.message = message;
+  let stack = err.stack;
   return err;
 };
 
-module.exports = {errorHandler}
+module.exports = errorHandler
