e78adec2c94e297f3cd5f417e8d8aa21a88d5f00
Ngozi <hexwhyzed01@gmail.com>
2026-02-05 08:43:52 +0100

    feat: Add the deposit function route

---
+++ routes/transaction.route.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const verifyToken = require("../middlewares/verify");
+const {
+  deposit,
+  transfer,
+  withdraw,
+} = require("../controllers/transaction.controller");
+const {
+  depositSchema,
+  transferSchema,
+  withdrawSchema,
+} = require("../validators/transaction.validator");
+
+const validator = require("express-joi-validation").createValidator({
+  passError: true,
+});
+
+const router = express.Router();
+
+router.post("/deposit", verifyToken, validator.body(depositSchema), deposit);
+router.post("/withdraw", validator.body(withdrawSchema), withdraw);
+router.post("/transfer", validator.body(transferSchema), transfer);
+
+module.exports = router;
