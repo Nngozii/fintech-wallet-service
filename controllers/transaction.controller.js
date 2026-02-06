7cef8da86f8b616baebb98e943d607d5147b0937
Ngozi <hexwhyzed01@gmail.com>
2026-02-06 17:02:10 +0100

    feat:Work on the deposit controller function

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -55,7 +55,7 @@ exports.deposit = async (req, res, next) => {
   } catch (err) {
     res.status(500).json({
       message: err.message,
-      stack: err.stack,
+      //stack: err.stack,
     });
   } finally {
     await session.endSession();
@@ -63,6 +63,72 @@ exports.deposit = async (req, res, next) => {
   }
 };
 
-exports.transfer = (req, res, next) => {};
+exports.transfer = async (req, res, next) => {
+  const { amount, recipient } = req.body;
+  //Start MongoDB session for atomic update
+  const session = await mongoose.startSession();
+
+  try {
+    await session.withTransaction(async () => {
+      const wallet = await Wallet.findById(req.user.walletID).session(session);
+      const recipientWallet = await Wallet.findById(recipient).session(session);
+
+      if (!wallet) {
+        throw new Error("Wallet not found");
+      }
+      if (!recipientWallet) {
+        throw new Error("Recipient wallet not found");
+      }
+      if (wallet.status !== "active") {
+        throw new Error("Wallet is not active");
+      }
+      if (recipientWallet.status !== "active") {
+        throw new Error("Recipient wallet is not active");
+      }
+      if(wallet._id.equals(recipientWallet._id)){
+        throw new Error("You cannot transfer to same wallet")
+      }
+      if (wallet.balance < amount) {
+        throw new Error("Insufficient fund");
+      }
+      //Record balance before transaction
+      let balanceBefore = wallet.balance;
+
+      let transaction = new Transaction({
+        userId: req.user.sub,
+        transactionType: "transfer",
+        amount,
+        sender: wallet,
+        recipient: recipientWallet._id,
+        balanceBefore,
+        status: "pending",
+        timestamp: new Date(),
+      });
+
+      //Atomic balance update
+      wallet.balance -= amount;
+      recipientWallet.balance += amount;
+      await wallet.save({ session });
+      await recipientWallet.save({ session });
+      //Balance after transaction
+      transaction.balanceAfter = wallet.balance;
+
+      transaction.status = "successful";
+      await transaction.save({ session });
+    });
+
+    return res
+      .status(200)
+      .send(`You have Successfully sent ${amount} to ${recipient}`);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      //stack: err.stack,
+    });
+  } finally {
+    await session.endSession();
+    console.log("session ended");
+  }
+};
 
 exports.withdraw = (req, res, next) => {};
